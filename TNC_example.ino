0ca62b4f1fa02a46eccd26e322648ee15bdf8bf0
N2WU <49031851+N2WU@users.noreply.github.com>
2021-06-21 21:35:59 -0400

    Confirm Physical Tests

--- TNC_example.ino
+++ TNC_example.ino
@@ -12,7 +12,7 @@ const int buttonPin = 2;
 
 // variables will change:
 bool buttonState = false;      
-
+int count = 0;
 void setup()
 {
   // initialize the pushbutton pin as an input:
@@ -24,22 +24,31 @@ void setup()
   
   //Set module as transmitter
   radio.stopListening();
+  Serial.begin(9600);
+
 }
 void loop()
 {
   //Send message to receiver
   buttonState = digitalRead(buttonPin);
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+
+  delay(10000);
+  buttonState = HIGH;
   if (buttonState == HIGH) {
-    if count == 0 {
+    if (count == 0) {
       const char text[] = "RELEASE BALLOON";
       radio.write(&text, sizeof(text));
+      Serial.print(text);
       count++;
+      delay(250);
+      buttonState = LOW;
       delay(1000);
     }
     else{
       const char text[] = "RESET BALLOON";
       radio.write(&text, sizeof(text));
+      Serial.print(text);
       count = count - 1;
       delay(1000);
     }
